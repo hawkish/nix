d7b138a76f15c471fab0a7ef33f269bce31cd59a
hawkish <mortenhogh@gmail.com>
2025-01-05 16:18:55 +0100

    zsh fuckup

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -5,38 +5,36 @@
 {
   programs = {
     home-manager.enable = true;
-    /*
-      zsh = {
-        enable = true;
-        enableCompletion = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-          gs = "git status";
-          gc = "git commit";
-          ga = "git add";
-          gd = "git diff";
-          gb = "git branch";
-          gl = "git log";
-          gsb = "git show-branch";
-          gco = "git checkout";
-          gg = "git grep";
-          gk = "gitk --all";
-          gr = "git rebase";
-          gri = "git rebase --interactive";
-          gcp = "git cherry-pick";
-          grm = "git rm";
-          grp = "git remote prune origin";
-          gp = "git push";
-        };
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-          ];
-          theme = "robbyrussell";
-        };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        gs = "git status";
+        gc = "git commit";
+        ga = "git add";
+        gd = "git diff";
+        gb = "git branch";
+        gl = "git log";
+        gsb = "git show-branch";
+        gco = "git checkout";
+        gg = "git grep";
+        gk = "gitk --all";
+        gr = "git rebase";
+        gri = "git rebase --interactive";
+        gcp = "git cherry-pick";
+        grm = "git rm";
+        grp = "git remote prune origin";
+        gp = "git push";
       };
-    */
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+        ];
+        theme = "robbyrussell";
+      };
+    };
     git = {
       enable = true;
       userEmail = "mortenhogh@gmail.com";
