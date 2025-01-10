8b3e86ae04a2cbe8c9d5d62c5e8633dca71751b3
hawkish <mortenhogh@gmail.com>
2025-01-10 16:10:09 +0100

    Fixed finder

--- modules/darwin/core/system.nix
+++ modules/darwin/core/system.nix
@@ -5,7 +5,8 @@
     configurationRevision = self.rev or self.dirtyRev or null;
     defaults = {
       finder.AppleShowAllExtensions = true;
-      finder.FXPreferredViewStyle = "clmv";
+      # List view in Finder
+      finder.FXPreferredViewStyle = "Nlmv";
       NSGlobalDomain.AppleInterfaceStyle = "Dark";
       ".GlobalPreferences"."com.apple.mouse.scaling" = 1.0;
       NSGlobalDomain.NSScrollAnimationEnabled = false;
