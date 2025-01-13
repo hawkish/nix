57881f35097e590a50f696a4c0ec2a19ec5f316d
Morten Høgh <mho@bankdata.dk>
2025-01-13 09:31:22 +0100

    Added hide_during_completion

--- packages/nvim/config/completion/cmp-copilot.nix
+++ packages/nvim/config/completion/cmp-copilot.nix
@@ -6,18 +6,7 @@
       autoTrigger = true;
       keymap.accept = "<S-CR>";
     };
+    hide_during_completion = false;
     panel.enabled = false;
   };
-
-  extraConfigLua = # Lua
-    ''
-      require("copilot").setup({
-        auto_trigger = true,
-        hide_during_completion = false,
-        keymap = {
-          accept = "<S-CR>",
-        },
-        panel = { enabled = false },
-      })
-    '';
 }
