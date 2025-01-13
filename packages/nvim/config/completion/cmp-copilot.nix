650fc82a7716a44144f6b947a0e840dfba9ac233
hawkish <mortenhogh@gmail.com>
2025-01-13 18:38:16 +0100

    Changed map

--- packages/nvim/config/completion/cmp-copilot.nix
+++ packages/nvim/config/completion/cmp-copilot.nix
@@ -17,7 +17,7 @@
         enabled = true;
         auto_trigger = true;
         hide_during_completion = false;
-        keymap.accept = "<S-CR>";
+        keymap.accept = "<S-S>";
       };
       panel.enabled = false;
     };
