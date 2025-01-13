64c6aa374d024d0328d602007f836a298b7c9902
hawkish <mortenhogh@gmail.com>
2025-01-13 18:51:35 +0100

    dsv

--- packages/nvim/config/completion/cmp-copilot.nix
+++ packages/nvim/config/completion/cmp-copilot.nix
@@ -17,7 +17,7 @@
         enabled = true;
         auto_trigger = true;
         hide_during_completion = false;
-        keymap.accept = "<TAB-CR>";
+        keymap.accept = "<CTRL-CR>";
       };
       panel.enabled = false;
     };
