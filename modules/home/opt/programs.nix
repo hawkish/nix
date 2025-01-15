5b4dd5bf1caf91365630da6ad07236d5a59edc29
hawkish <mortenhogh@gmail.com>
2025-01-15 19:38:06 +0100

    Changed settings for alacritty

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -44,45 +44,6 @@
       enable = true;
       settings = {
         live_config_reload = true;
-        colors = {
-          primary = {
-            background = "#1E1F1C";
-          };
-          normal = {
-            black = "#333333";
-            red = "#C4265E";
-            green = "#86B42B";
-            yellow = "#B3B42B";
-            blue = "#6A7EC8";
-            magenta = "#8C6BC8";
-            cyan = "#56ADBC";
-            white = "#e3e3dd";
-          };
-          bright = {
-            black = "#666666";
-            red = "#f92672";
-            green = "#A6E22E";
-            yellow = "#e2e22e";
-            blue = "#819aff";
-            magenta = "#AE81FF";
-            cyan = "#66D9EF";
-            white = "#f8f8f2";
-          };
-          dim = {
-            black = "#1c1c1c";
-            red = "#ff6565";
-            green = "#93d44f";
-            yellow = "#eab93d";
-            blue = "#204a87";
-            magenta = "#ce5c00";
-            cyan = "#89b6e2";
-            white = "#cccccc";
-          };
-          cursor = {
-            cursor = "0xd8d8d8";
-            text = "0x181818";
-          };
-        };
 
         cursor = {
           style = "Beam";
@@ -96,32 +57,8 @@
 
         shell = {
           program = "${pkgs.zsh}/bin/zsh";
-          args = [
-            "--login"
-            "-c"
-            "tmux new-session -A -s main-alacritty"
-          ];
         };
 
-        font = {
-          normal = {
-            family = "Hack Nerd Font Mono";
-            style = "Regular";
-          };
-          italic = {
-            family = "Hack Nerd Font Mono";
-            style = "Italic";
-          };
-          bold_italic = {
-            family = "Hack Nerd Font Mono";
-            style = "Italic";
-          };
-          bold = {
-            family = "Hack Nerd Font Mono";
-            style = "Bold";
-          };
-          size = 13.0;
-        };
         keyboard.bindings = [
           {
             key = "Return";
@@ -199,16 +136,6 @@
           }
         ];
 
-        window = {
-          padding = {
-            x = 2;
-            y = 2;
-          };
-          decorations = "Buttonless";
-          dynamic_padding = true;
-          dynamic_title = true;
-          startup_mode = "Maximized";
-        };
       };
     };
   };
