e8ad41d5a0af348dc4f3c7ab843bdf14a76b97a1
hawkish <mortenhogh@gmail.com>
2025-01-15 19:46:25 +0100

    Alacritty change

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -63,13 +63,13 @@
           {
             key = "Return";
             mods = "Shift";
-            action = "\\u001B[13;2u";
+            chars = "\\u001B[13;2u";
           }
 
           {
             key = "Return";
             mods = "Control";
-            action = "\\u001B[13;5u";
+            chars = "\\u001B[13;5u";
           }
 
           # ⌘ + enter puts window in macOS full screen
