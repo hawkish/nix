6b9b68da53df8777fb679abb82b15859b25ad96b
hawkish <mortenhogh@gmail.com>
2025-01-15 20:28:53 +0100

    wezterm added

---
+++ modules/home/opt/modules/wezterm.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs = {
+    wezterm = {
+      enable = true;
+      colorSchemes = {
+        enur = {
+          foreground = "#FFFFFF";
+          background = "#000000";
+
+          cursor_fg = "#000000";
+          cursor_bg = "#FFFFFF";
+          cursor_border = "#FFFFFF";
+
+          selection_fg = "#FFFFFF";
+          selection_bg = "#44475a";
+
+          ansi = [
+            "#000000"
+            "#FF3C3C"
+            "#00FF00"
+            "#FFFF5A"
+            "#4F9DFF"
+            "#FF4FD8"
+            "#4DFFFF"
+            "#CCCCCC"
+          ];
+          brights = [
+            "#7F7F7F"
+            "#FF7373"
+            "#32FF32"
+            "#FFFF82"
+            "#74AFFF"
+            "#FF74E3"
+            "#73FFFF"
+            "#FFFFFF"
+          ];
+        };
+      };
+      extraConfig = # Lua
+        ''
+          return {
+            color_scheme = "enur",
+            front_end = "WebGpu"
+          }
+        '';
+    };
+  };
+}
