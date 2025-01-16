d8d535bb9df1a12160fa99e500592d145c483f5a
hawkish <mortenhogh@gmail.com>
2025-01-16 17:13:27 +0100

    Fixed scheme

--- modules/home/opt/modules/wezterm.nix
+++ modules/home/opt/modules/wezterm.nix
@@ -8,46 +8,12 @@
     wezterm = {
       enable = true;
       enableZshIntegration = true;
-      colorSchemes = {
-        enur = {
-          foreground = "#FFFFFF";
-          background = "#000000";
-
-          cursor_fg = "#000000";
-          cursor_bg = "#FFFFFF";
-          cursor_border = "#FFFFFF";
-
-          selection_fg = "#FFFFFF";
-          selection_bg = "#44475a";
-
-          ansi = [
-            "#000000"
-            "#FF3C3C"
-            "#00FF00"
-            "#FFFF5A"
-            "#4F9DFF"
-            "#FF4FD8"
-            "#4DFFFF"
-            "#CCCCCC"
-          ];
-          brights = [
-            "#7F7F7F"
-            "#FF7373"
-            "#32FF32"
-            "#FFFF82"
-            "#74AFFF"
-            "#FF74E3"
-            "#73FFFF"
-            "#FFFFFF"
-          ];
-        };
-      };
       extraConfig = # Lua
         ''
           local wezterm = require 'wezterm';
           local config = wezterm.config_builder()
 
-          config.color_scheme = "Catppuccin Mochar"
+          config.color_scheme = "Catppuccin Mocha"
           config.font = wezterm.font "JetBrainsMono Nerd Font Mono"
           config.font_size = 10.0
           config.window_decorations = "TITLE | RESIZE"
