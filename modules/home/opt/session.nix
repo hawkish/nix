e5dd3aafa3b7f23db29b1a97793f543d059fdd8e
hawkish <mortenhogh@gmail.com>
2025-01-21 20:23:42 +0100

    Added path for gems

--- modules/home/opt/session.nix
+++ modules/home/opt/session.nix
@@ -5,6 +5,9 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
       JAVA_HOME = "${pkgs.jdk23.home}";
+      GEM_HOME = "$HOME/.gem";
+      GEM_PATH = "$HOME/.gem";
     };
+    sessionPath = [ "$GEM_HOME/bin" ];
   };
 }
