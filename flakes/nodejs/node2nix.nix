c1370a49041dc44d3a8262dd0d37f965cd453129
Morten Høgh <mortenhogh@gmail.com>
2025-01-28 21:21:42 +0100

    Added flakes

--- flakes/nodejs/node2nix.nix
+++
@@ -1,5 +0,0 @@
-with import <nixpkgs> { };
-stdenv.mkDerivation rec {
-  name = "node2nix";
-  buildInputs = [ nodePackages.node2nix ];
-}
---
+++ flakes/python/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Demo Nix dev environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # 3.12.0 release
+    python-nixpkgs.url = "github:NixOS/nixpkgs/e2b8feae8470705c3f331901ae057da3095cea10";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      python-nixpkgs,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        python-nixpkgs = inputs.python-nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = python-nixpkgs.mkShell {
+          packages = [
+            python-nixpkgs.python312
+          ];
+
+          shellHook = ''
+            python --version
+          '';
+        };
+      }
+    );
+}
