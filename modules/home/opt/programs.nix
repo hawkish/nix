1612156ffc03115ecc69631df26c412e6b29e022
hawkish <mortenhogh@gmail.com>
2025-02-10 18:07:22 +0100

    Removed extension

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -73,9 +73,6 @@
           # Prefer dark theme
           "layout.css.prefers-color-scheme.content-override" = 0; # 0: Dark, 1: Light, 2: Auto
         };
-        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-          ublock-origin
-        ];
         search = {
           force = true;
           default = "DuckDuckGo";
