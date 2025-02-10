21af673f5d7877b9a86a3ba0226dfb4db1cd7274
hawkish <mortenhogh@gmail.com>
2025-02-11 00:06:12 +0100

    Moved options

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -6,6 +6,54 @@
   ...
 }:
 {
+  config = lib.mkIf config.opt.programs.firefox.enable {
+    programs.firefox = {
+      enable = true;
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+          settings = {
+            "browser.startup.homepage" = "https://duckduckgo.com";
+            "browser.search.defaultenginename" = "DuckDuckGo";
+            "browser.search.order.1" = "DuckDuckGo";
+            "signon.rememberSignons" = false;
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.compactmode.show" = true;
+            "browser.cache.disk.enable" = false;
+            "widget.disable-workspace-management" = true;
+            "mousewheel.default.delta_multiplier_x" = 70;
+            "mousewheel.default.delta_multiplier_y" = 70;
+            "mousewheel.default.delta_multiplier_z" = 70;
+            "browser.newtabpage.pinned" = [
+              {
+                "label" = "GitHub";
+                "url" = "https://github.com";
+              }
+              {
+                "label" = "YouTube";
+                "url" = "https://youtube.com";
+              }
+            ];
+          };
+          search = {
+            force = true;
+            default = "DuckDuckGo";
+            order = [
+              "DuckDuckGo"
+              "Google"
+            ];
+          };
+          extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+            ublock-origin
+          ];
+        };
+      };
+    };
+  };
+
   programs = {
     home-manager.enable = true;
     zsh = {
@@ -42,53 +90,6 @@
       enable = true;
       userEmail = "mortenhogh@gmail.com";
       userName = "hawkish";
-    };
-    config = lib.mkIf config.opt.programs.firefox.enable {
-      firefox = {
-        enable = true;
-        profiles = {
-          default = {
-            id = 0;
-            name = "default";
-            isDefault = true;
-            settings = {
-              "browser.startup.homepage" = "https://duckduckgo.com";
-              "browser.search.defaultenginename" = "DuckDuckGo";
-              "browser.search.order.1" = "DuckDuckGo";
-              "signon.rememberSignons" = false;
-              "widget.use-xdg-desktop-portal.file-picker" = 1;
-              "browser.aboutConfig.showWarning" = false;
-              "browser.compactmode.show" = true;
-              "browser.cache.disk.enable" = false;
-              "widget.disable-workspace-management" = true;
-              "mousewheel.default.delta_multiplier_x" = 70;
-              "mousewheel.default.delta_multiplier_y" = 70;
-              "mousewheel.default.delta_multiplier_z" = 70;
-              "browser.newtabpage.pinned" = [
-                {
-                  "label" = "GitHub";
-                  "url" = "https://github.com";
-                }
-                {
-                  "label" = "YouTube";
-                  "url" = "https://youtube.com";
-                }
-              ];
-            };
-            search = {
-              force = true;
-              default = "DuckDuckGo";
-              order = [
-                "DuckDuckGo"
-                "Google"
-              ];
-            };
-            extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-              ublock-origin
-            ];
-          };
-        };
-      };
     };
     chromium = {
       enable = true;
