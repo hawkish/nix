550fb47c8adc4f8e022c81883b26a81adcb7b447
hawkish <mortenhogh@gmail.com>
2025-02-10 18:06:00 +0100

    Added firefox

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -42,82 +42,81 @@
       userName = "hawkish";
     };
 
-    /*
-      firefox = {
-        enable = true;
-        profiles.default = {
-          id = 0;
-          name = "Default";
-          isDefault = true;
-          settings = {
-            # Disable about:config warning
-            "browser.aboutConfig.showWarning" = false;
+    firefox = {
+      enable = true;
+      profiles.default = {
+        id = 0;
+        name = "Default";
+        isDefault = true;
+        settings = {
+          # Disable about:config warning
+          "browser.aboutConfig.showWarning" = false;
 
-            # Mozilla telemetry
-            "toolkit.telemetry.enabled" = true;
+          # Mozilla telemetry
+          "toolkit.telemetry.enabled" = true;
 
-            # Homepage settings
-            # 0 = blank, 1 = home, 2 = last visited page, 3 = resume previous session
-            "browser.startup.homepage" = "https://duckduckgo.com";
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.order.1" = "DuckDuckGo";
-            "browser.newtabpage.pinned" = [
-              {
-                "label" = "GitHub";
-                "url" = "https://github.com";
-              }
-              {
-                "label" = "YouTube";
-                "url" = "https://youtube.com";
-              }
-            ];
-            # Prefer dark theme
-            "layout.css.prefers-color-scheme.content-override" = 0; # 0: Dark, 1: Light, 2: Auto
-          };
-          extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-            ublock-origin
+          # Homepage settings
+          # 0 = blank, 1 = home, 2 = last visited page, 3 = resume previous session
+          "browser.startup.homepage" = "https://duckduckgo.com";
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
+          "browser.newtabpage.pinned" = [
+            {
+              "label" = "GitHub";
+              "url" = "https://github.com";
+            }
+            {
+              "label" = "YouTube";
+              "url" = "https://youtube.com";
+            }
           ];
-          search = {
-            force = true;
-            default = "DuckDuckGo";
-            order = [
-              "DuckDuckGo"
-              "Google"
-            ];
+          # Prefer dark theme
+          "layout.css.prefers-color-scheme.content-override" = 0; # 0: Dark, 1: Light, 2: Auto
+        };
+        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+          ublock-origin
+        ];
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = [
+            "DuckDuckGo"
+            "Google"
+          ];
 
-            engines = {
-              "Nix Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {
-                        name = "type";
-                        value = "packages";
-                      }
-                      {
-                        name = "query";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                  }
-                ];
-                icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@np" ];
-              };
-              "NixOS Wiki" = {
-                urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@nw" ];
-              };
-              "Bing".metaData.hidden = true;
-              "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
             };
+            "NixOS Wiki" = {
+              urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+            "Bing".metaData.hidden = true;
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
           };
         };
       };
-    */
+    };
+
     chromium = {
       enable = true;
       package = pkgs.brave;
