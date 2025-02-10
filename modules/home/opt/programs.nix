6a255b082e632f20878d79d905d16509e79b6038
hawkish <mortenhogh@gmail.com>
2025-02-10 17:47:59 +0100

    Fixed typo

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -72,7 +72,7 @@
           # Prefer dark theme
           "layout.css.prefers-color-scheme.content-override" = 0; # 0: Dark, 1: Light, 2: Auto
         };
-        extensions = with inputs.firefox-addons; [
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
         ];
         search = {
