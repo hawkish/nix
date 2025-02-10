3da7e669d6d770c2df238d3ca2c2b26c22fc5fe0
hawkish <mortenhogh@gmail.com>
2025-02-11 00:15:15 +0100

    Moved firefox

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -1,59 +1,8 @@
 {
   pkgs,
-  inputs,
-  config,
-  lib,
   ...
 }:
 {
-  config = lib.mkIf config.opt.programs.firefox.enable {
-    programs.firefox = {
-      enable = true;
-      profiles = {
-        default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          settings = {
-            "browser.startup.homepage" = "https://duckduckgo.com";
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.order.1" = "DuckDuckGo";
-            "signon.rememberSignons" = false;
-            "widget.use-xdg-desktop-portal.file-picker" = 1;
-            "browser.aboutConfig.showWarning" = false;
-            "browser.compactmode.show" = true;
-            "browser.cache.disk.enable" = false;
-            "widget.disable-workspace-management" = true;
-            "mousewheel.default.delta_multiplier_x" = 70;
-            "mousewheel.default.delta_multiplier_y" = 70;
-            "mousewheel.default.delta_multiplier_z" = 70;
-            "browser.newtabpage.pinned" = [
-              {
-                "label" = "GitHub";
-                "url" = "https://github.com";
-              }
-              {
-                "label" = "YouTube";
-                "url" = "https://youtube.com";
-              }
-            ];
-          };
-          search = {
-            force = true;
-            default = "DuckDuckGo";
-            order = [
-              "DuckDuckGo"
-              "Google"
-            ];
-          };
-          extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-            ublock-origin
-          ];
-        };
-      };
-    };
-  };
-
   programs = {
     home-manager.enable = true;
     zsh = {
