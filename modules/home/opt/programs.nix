56fcf7ae6c4c2fc7bf5f8b20ba03edc103000b40
hawkish <mortenhogh@gmail.com>
2025-02-10 20:05:57 +0100

    Added extra delta

--- modules/home/opt/programs.nix
+++ modules/home/opt/programs.nix
@@ -59,9 +59,9 @@
             "browser.compactmode.show" = true;
             "browser.cache.disk.enable" = false;
             "widget.disable-workspace-management" = true;
-            "mousewheel.default.delta_multiplier_x" = 20;
-            "mousewheel.default.delta_multiplier_y" = 20;
-            "mousewheel.default.delta_multiplier_z" = 20;
+            "mousewheel.default.delta_multiplier_x" = 50;
+            "mousewheel.default.delta_multiplier_y" = 50;
+            "mousewheel.default.delta_multiplier_z" = 50;
             "browser.newtabpage.pinned" = [
               {
                 "label" = "GitHub";
