573aac6f21cabb4f919f087ac81fe2cf87b5ad5f
hawkish <mortenhogh@gmail.com>
2025-03-27 20:27:07 +0100

    Added work

--- modules/home/opt/options.nix
+++ modules/home/opt/options.nix
@@ -1,6 +1,9 @@
 { lib, ... }:
 {
   options.opt = with lib; {
+    features = {
+      work.enable = mkEnableOption "Enable work features";
+    };
     programs = {
       "1password".enable = mkEnableOption "Enable 1Password";
       firefox.enable = mkEnableOption "Enable Firefox";
