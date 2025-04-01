1c82be950a5069e50351e93ec2675ad7270dec09
hawkish <age1mzjnjcltj9wrka2pdyzw6qcuuvkp8k6d5hdjm6435ss4zdnsyppqzd98n6>
2025-04-01 20:43:05 +0200

    Added sops template

--- modules/home/opt/programs/git.nix
+++ modules/home/opt/programs/git.nix
@@ -1,13 +1,36 @@
 {
   config,
+  lib,
   ...
 }:
 {
-  programs = {
-    git = {
-      enable = true;
-      userEmail = config.sops.secrets.private_email.path;
-      userName = "hawkish";
+  config = {
+    sops = {
+      secrets = {
+        private_email = { };
+        work_email = lib.mkIf (config.opt.features.work.enable) { };
+      };
+
+      templates = {
+        # Private PC
+        private-git-config.content = lib.generators.toINI { } {
+          user = {
+            email = config.sops.placeholder.private_email;
+            name = "hawkish";
+          };
+        };
+      };
+    };
+    programs = {
+      git = {
+        enable = true;
+        includes = ([
+          # Private
+          {
+            path = config.sops.templates.private-git-config.path;
+          }
+        ]);
+      };
     };
   };
 }
