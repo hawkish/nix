20436d5a0cd1c73e8ba95c366c4069e12953b2bd
hawkish <mortenhogh@gmail.com>
2025-04-04 17:11:25 +0200

    Fixing ssh

--- modules/home/opt/programs/ssh.nix
+++ modules/home/opt/programs/ssh.nix
@@ -24,7 +24,7 @@ in
           Host github.com
             HostName github.com
             User hawkish
-            IdentityAgent ${onePasswordSock}
+            IdentityAgent "${onePasswordSock}"
         '';
 
         work-ssh-config.content = ''
@@ -34,7 +34,7 @@ in
           Host github.com
             HostName github.com
             User ${config.sops.placeholder.work_email}
-            IdentityAgent ${onePasswordSock}
+            IdentityAgent "${onePasswordSock}"
           Host github.com-personal
             HostName github.com
             User hawkish
