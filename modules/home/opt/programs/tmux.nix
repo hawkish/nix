a95c7eec1383f2a949724228878938fae1bb0a4b
hawkish <mortenhogh@gmail.com>
2025-04-05 17:22:23 +0200

    tmux config

--- modules/home/opt/programs/tmux.nix
+++ modules/home/opt/programs/tmux.nix
@@ -17,19 +17,8 @@
       tmuxPlugins.resurrect
       tmuxPlugins.continuum
       tmuxPlugins.sensible
-      tmuxPlugins.dracula
     ];
     extraConfig = ''
-      # Config Dracula Theme
-      set -g @dracula-show-fahrenheit false
-      set -g @dracula-show-powerline true
-      set -g @dracula-show-network false
-      set -g @dracula-show-left-icon session
-      set -g @dracula-cpu-usage true
-      set -g @dracula-ram-usage true
-      set -g @dracula-day-month true
-      set -g @dracula-military-time true
-      set -g @dracula-show-flags true 
 
       set -g default-terminal tmux-256color
 
