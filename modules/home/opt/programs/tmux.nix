8534c559293abd5b0d1c261d1885c4f0bd44b3df
hawkish <mortenhogh@gmail.com>
2025-04-05 18:52:49 +0200

    tmux config

--- modules/home/opt/programs/tmux.nix
+++ modules/home/opt/programs/tmux.nix
@@ -10,13 +10,14 @@
     clock24 = true;
     newSession = true;
     keyMode = "vi";
-    shortcut = "b";
+    shortcut = "t";
     escapeTime = 0;
     shell = lib.getExe pkgs.zsh;
     plugins = with pkgs; [
       tmuxPlugins.resurrect
       tmuxPlugins.continuum
       tmuxPlugins.sensible
+      tmuxPlugins.better-mouse-mode
     ];
     extraConfig = ''
 
@@ -26,6 +27,13 @@
       set -as terminal-features 'xterm*:extkeys'
       set -s extended-keys on
       set-option -g renumber-windows on
+
+      # Mouse works as expected
+      set-option -g mouse on
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
 
     '';
   };
