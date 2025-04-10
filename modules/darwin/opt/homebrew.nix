7c76aa036b8f3bf8c33edaf12c600a674db11ba3
hawkish <mortenhogh@gmail.com>
2025-04-10 08:49:19 +0200

    Add ruby build dependencies to homebrew

--- modules/darwin/opt/homebrew.nix
+++ modules/darwin/opt/homebrew.nix
@@ -8,16 +8,27 @@ let
       [
         "chruby"
         "ruby-install"
+        "autoconf"
+        "automake"
+        "bison"
+        "ca-certificates"
+        "libffi"
+        "libyaml"
+        "m4"
+        "openssl@3"
+        "readline"
+        "xz"
       ]
     else
       [ ];
   personalBrew = if config.opt.features.personal.enable then [ ] else [ ];
+  sharedBrew = [ ];
 in
 {
   homebrew = {
     enable = true;
     onActivation.cleanup = "uninstall";
-    brews = workBrew ++ personalBrew;
+    brews = workBrew ++ personalBrew ++ sharedBrew;
     casks = [
       "brave-browser"
       "1Password"
