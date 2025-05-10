e96a8464cbd191e81cc63a8793f6cc00881c00c9
hawkish <mortenhogh@gmail.com>
2025-05-10 21:12:46 +0200

    Added 1password extension

--- modules/home/opt/programs/firefox.nix
+++ modules/home/opt/programs/firefox.nix
@@ -77,6 +77,11 @@
                 "url" = "https://youtube.com";
               }
             ];
+            # 1Password:
+            "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+            };
             "extensions.ublock-origin.enabled" = true;
           };
           search = {
@@ -89,7 +94,6 @@
           };
           extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
             ublock-origin
-            "1password"
           ];
         };
       };
