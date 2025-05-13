7134ff64e829e00d5ecac49bbe2d6bf94462013a
hawkish <mortenhogh@gmail.com>
2025-05-13 18:42:53 +0200

    Deleted move lines

--- packages/nvim/config/keymap.nix
+++ packages/nvim/config/keymap.nix
@@ -144,55 +144,6 @@
       };
     }
 
-    # Move lines
-    {
-      mode = "n";
-      key = "<A-j>";
-      action = "<cmd>m .+1<cr>==";
-      options = {
-        desc = "Move up";
-      };
-    }
-    {
-      mode = "n";
-      key = "<A-k>";
-      action = "<cmd>m .-2<cr>==";
-      options = {
-        desc = "Move down";
-      };
-    }
-    {
-      mode = "i";
-      key = "<A-j>";
-      action = "<esc><cmd>m .+1<cr>==gi";
-      options = {
-        desc = "Move up";
-      };
-    }
-    {
-      mode = "i";
-      key = "<A-k>";
-      action = "<esc><cmd>m .-2<cr>==gi";
-      options = {
-        desc = "Move down";
-      };
-    }
-    {
-      mode = "v";
-      key = "<A-j>";
-      action = ":m '>+1<cr>gv=gv";
-      options = {
-        desc = "Move up";
-      };
-    }
-    {
-      mode = "v";
-      key = "<A-k>";
-      action = ":m '>-2<cr>gv=gv";
-      options = {
-        desc = "Move down";
-      };
-    }
     # Better indenting
     {
       mode = "v";
