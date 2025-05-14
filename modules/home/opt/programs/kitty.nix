87d1fea0dd263b0cf1cd45ea8d268e901c09ce6d
hawkish <mortenhogh@gmail.com>
2025-05-14 19:38:32 +0200

    More kitty settings

--- modules/home/opt/programs/kitty.nix
+++ modules/home/opt/programs/kitty.nix
@@ -3,33 +3,12 @@
 
   home.file.kitty-theme = {
     enable = true;
-    target = ".config/kitty/themes/enur.conf";
-    text = ''
-      foreground #FFFFFF
-      background #000000
-
-      cursor #000000
-      cursor_text #FFFFFF
-      selection #44475a
-      selection_text #FFFFFF
-
-      color0 #000000
-      color1 #FF3C3C
-      color2 #00FF00
-      color3 #FFFF5A
-      color4 #4F9DFF
-      color5 #FF4FD8
-      color6 #4DFFFF
-      color7 #CCCCCC
-      color8 #7F7F7F
-      color9 #FF7373
-      color10 #32FF32
-      color11 #FFFF82
-      color12 #74AFFF
-      color13 #FF74E3
-      color14 #73FFFF
-      color15 #FFFFFF
-    '';
+    baseIndex = 1;
+    clock24 = true;
+    keyMode = "vi";
+    newSession = true;
+    shortcut = "t";
+    escapeTime = 0;
   };
   programs.kitty = {
     enable = true;
