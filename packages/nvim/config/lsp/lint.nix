4181e97ca807eefa4e460b5ce6bf13bf14d1e9e5
hawkish <mortenhogh@gmail.com>
2025-05-24 20:06:58 +0200

    Fixed typo

--- packages/nvim/config/lsp/lint.nix
+++ packages/nvim/config/lsp/lint.nix
@@ -16,16 +16,16 @@
         "vale"
       ];
     };
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>ml";
-        action = "<cmd>lua require('lint').try_lint()<cr>";
-        options = {
-          desc = "Lint file";
-          silent = true;
-        };
-      }
-    ];
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ml";
+      action = "<cmd>lua require('lint').try_lint()<cr>";
+      options = {
+        desc = "Lint file";
+        silent = true;
+      };
+    }
+  ];
 }
