784bdef40663a957f442feaad192b0c5d2a3e6fb
hawkish <mortenhogh@gmail.com>
2025-05-24 20:05:24 +0200

    Added keymap to lint

--- packages/nvim/config/lsp/lint.nix
+++ packages/nvim/config/lsp/lint.nix
@@ -16,5 +16,16 @@
         "vale"
       ];
     };
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>ml";
+        action = "<cmd>lua require('lint').try_lint()<cr>";
+        options = {
+          desc = "Lint file";
+          silent = true;
+        };
+      }
+    ];
   };
 }
