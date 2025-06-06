c91a8b5a6c0a3d4417c0ba360f5b019336754fae
hawkish <mortenhogh@gmail.com>
2025-06-06 14:05:14 +0200

    Removed options

--- packages/nvim/config/options.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  # Define the options structure to match the system options
-  options.opt.features.personal.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = true; # Default to enabled for simplicity
-    description = "Enable personal features in Neovim";
-  };
-}
