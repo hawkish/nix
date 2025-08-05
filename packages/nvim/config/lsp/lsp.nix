8c9267a9e3acefb42622f58c893a3d67f0ab366c
hawkish <mortenhogh@gmail.com>
2025-08-05 17:39:10 +0200

    Fixing rust analyzer

--- packages/nvim/config/lsp/lsp.nix
+++ packages/nvim/config/lsp/lsp.nix
@@ -71,8 +71,8 @@ in
         };
         rust_analyzer = {
           enable = true;
-          installCargo = true;
-          installRustc = true;
+          installCargo = false;
+          installRustc = false;
         };
       };
       keymaps = {
