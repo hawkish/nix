f5b0de361c0c0d4f30473615f4ca11a43c8746e9
hawkish <mortenhogh@gmail.com>
2025-10-04 19:45:15 +0200

    Add unstable overlay to Darwin configuration
    Fix undefined variable 'unstable' error by adding nixpkgs-unstable overlay to Darwin nix configuration, matching the existing NixOS setup.

--- modules/darwin/core/nix.nix
+++ modules/darwin/core/nix.nix
@@ -1,5 +1,16 @@
+{ inputs, ... }:
 {
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [
+      (self: _super: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit (self) system;
+          config.allowUnfree = true;
+        };
+      })
+    ];
+  };
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
