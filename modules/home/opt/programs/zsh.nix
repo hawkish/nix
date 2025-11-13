dd2effffb9a0201415c0474aececd78524d19024
hawkish <mortenhogh@gmail.com>
2025-11-13 10:06:07 +0100

    Refactoring

--- modules/home/opt/programs/zsh.nix
+++ modules/home/opt/programs/zsh.nix
@@ -5,34 +5,27 @@
   ...
 }:
 let
-  workContent =
-    if config.opt.features.work.enable then
-      ''
-        # Ruby gem installation
-        export GEM_HOME="$HOME/.gem"
-        export GEM_PATH="$HOME/.gem"
-        export PATH=$GEM_HOME/bin:$PATH
+  workContent = lib.optionalString config.opt.features.work.enable ''
+    # Ruby gem installation
+    export GEM_HOME="$HOME/.gem"
+    export GEM_PATH="$HOME/.gem"
+    export PATH=$GEM_HOME/bin:$PATH
 
-        # Chruby installation
-        # enable chruby and select Ruby version
-        source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
-        source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
-        # chruby ruby-3.1.2
-        chruby 3.4.2
+    # Chruby installation
+    # enable chruby and select Ruby version
+    source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+    source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
+    chruby 3.4.2
 
-        # X3270 installation
-        export PATH=${pkgs.x3270}/bin:$PATH
-      ''
-    else
-      "";
-  personalContent =
-    if config.opt.features.personal.enable then
-      ''
-        # Extra commands
-        # Other extras
-      ''
-    else
-      "";
+    # X3270 installation
+    export PATH=${pkgs.x3270}/bin:$PATH
+  '';
+
+  personalContent = lib.optionalString config.opt.features.personal.enable ''
+    # Extra commands
+    # Other extras
+  '';
+
   commonContent = ''
     # Java installation
     export JAVA_HOME_17="${pkgs.jdk17.home}"
@@ -44,45 +37,43 @@ let
     export PATH=$HOME/git/rustycommander/target/release:$PATH
   '';
 
-  initContent = lib.concatStringsSep "\n" [
-    workContent
-    personalContent
-    commonContent
-  ];
-
 in
 {
-  programs = {
-    zsh = {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      gs = "git status";
+      gc = "git commit";
+      ga = "git add";
+      gd = "git diff";
+      gb = "git branch";
+      gl = "git log";
+      gsb = "git show-branch";
+      gco = "git checkout";
+      gg = "git grep";
+      gk = "gitk --all";
+      gr = "git rebase";
+      gri = "git rebase --interactive";
+      gcp = "git cherry-pick";
+      grm = "git rm";
+      grp = "git remote prune origin";
+      gp = "git push";
+    };
+
+    # Use initExtra (standard option) instead of initContent
+    initExtra = lib.concatStringsSep "\n" [
+      workContent
+      personalContent
+      commonContent
+    ];
+
+    oh-my-zsh = {
       enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-        gs = "git status";
-        gc = "git commit";
-        ga = "git add";
-        gd = "git diff";
-        gb = "git branch";
-        gl = "git log";
-        gsb = "git show-branch";
-        gco = "git checkout";
-        gg = "git grep";
-        gk = "gitk --all";
-        gr = "git rebase";
-        gri = "git rebase --interactive";
-        gcp = "git cherry-pick";
-        grm = "git rm";
-        grp = "git remote prune origin";
-        gp = "git push";
-      };
-      initContent = initContent;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "git"
-        ];
-        theme = "robbyrussell";
-      };
+      plugins = [ "git" ];
+      theme = "robbyrussell";
     };
   };
 }
--- modules/home/opt/session.nix
+++ modules/home/opt/session.nix
@@ -6,11 +6,6 @@
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
-    #JAVA_HOME_17 = "${pkgs.jdk17.home}";
-    #JAVA_HOME_21 = "${pkgs.jdk21.home}";
-    #JAVA_HOME = "${pkgs.jdk21.home}";
-    GEM_HOME = "$HOME/.gem";
-    GEM_PATH = "$HOME/.gem";
     SOPS_AGE_KEY_FILE = "${homeDir}/.config/sops/age/keys.txt";
   };
   home.sessionPath = [
